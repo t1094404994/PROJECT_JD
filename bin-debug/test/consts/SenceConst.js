29dac3c1a07c2dec3883026743815eff162d1069
txz <t1094404@live.com>
2020-03-03 09:49:22 +0800

    remove debug and release Files

--- bin-debug/test/consts/SenceConst.js
+++
@@ -1,9 +0,0 @@
-var SenceConst;
-(function (SenceConst) {
-    //进入界面
-    SenceConst[SenceConst["LOGIN"] = 0] = "LOGIN";
-    //测试界面1
-    SenceConst[SenceConst["TEST1"] = 1] = "TEST1";
-    //测试界面2
-    SenceConst[SenceConst["TEST2"] = 2] = "TEST2";
-})(SenceConst || (SenceConst = {}));
